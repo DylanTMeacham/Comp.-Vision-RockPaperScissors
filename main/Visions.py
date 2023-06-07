6d1f16c32f80e560311be875069dcadba9ee98ce
Dylan Meacham <nessy7005@gmail.com>
2023-06-07 18:22:53 -0400

    Did SOME stuff

--- main/Visions.py
+++ main/Visions.py
@@ -1,10 +1,46 @@
 
-import cv2
+import cv2 
+import mediapipe as mp 
+import argparse
 
-# Initialize the webcam
-cap = cv2.VideoCapture(0)
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", type=int, default=1)
+    parser.add_argument('--use_static_image_mode', action='store_true')
+    parser.add_argument("--min_detection_confidence",
+                        help='min_detection_confidence',
+                        type=float,
+                        default=0.7)
+    parser.add_argument("--min_tracking_confidence",
+                        help='min_tracking_confidence',
+                        type=int,
+                        default=0.5)
+
+    args = parser.parse_args()
+
+    return args
 
 def _in_Visions():
+    args = get_args()
+
+    #Initializes the Camera 
+    cam = args.device
+    cap = cv2.VideoCapture(cam)
+
+    #use_static_image_mode = args.use_static_image_mode
+    min_detection_confidence = args.min_detection_confidence
+    #min_tracking_confidence = args.min_tracking_confidence
+
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(
+        #static_image_mode=use_static_image_mode,
+        max_num_hands=2,
+        min_detection_confidence=min_detection_confidence,
+        #min_tracking_confidence=min_tracking_confidence,
+    )
+
+
     while True:
 
         _, frame = cap.read()
