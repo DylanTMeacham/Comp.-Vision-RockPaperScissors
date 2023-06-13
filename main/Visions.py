b6ee817e32e7696b5854eb8629437a125ea4397c
Dylan Meacham <nessy7005@gmail.com>
2023-06-13 18:23:54 -0400

    Made Data Collection Folders

--- main/Visions.py
+++ main/Visions.py
@@ -44,4 +44,27 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
     cap.release()
     cv.destroyAllWindows()
 
-#Stopped at part 3
+def extract_keypoints(results):
+    right_hand = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    left_hand = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([right_hand, left_hand])
+
+#Path for exported data (np arrays)
+DATA_PATH = os.path.join('MP_Data')
+
+#Actions we try to detect
+actions = np.array(['rock', 'paper', 'scissors'])
+
+#Action Detection - rather than one frame I used 30 video worth of data to predict actions 
+no_sequences = 30
+
+#Videos are going to be 30 frames in length
+sequence_length = 30
+
+#Creates a folder for each action and creates a subfolder in that folder for each sequence
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
