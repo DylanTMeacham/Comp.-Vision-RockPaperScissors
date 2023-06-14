3925cd3215d3cf80aa6310e1d4b799764dbaaedb
Dylan Meacham <nessy7005@gmail.com>
2023-06-14 19:11:28 -0400

    made wait and collection logic

--- main/Data.py
+++ main/Data.py
@@ -1,18 +1,19 @@
-import os
 import numpy as np
+import os
+
 #Path for exported data (np arrays)
 DATA_PATH = os.path.join('MP_Data')
 
-#Actions we try to detect
+# Actions we try to detect
 actions = np.array(['rock', 'paper', 'scissors'])
 
-#Action Detection - rather than one frame I used 30 video worth of data to predict actions 
+# Action Detection - rather than one frame I used 30 video worth of data to predict actions 
 no_sequences = 30
 
-#Videos are going to be 30 frames in length
+# Videos are going to be 30 frames in length
 sequence_length = 30
 
-#Creates a folder for each action and creates a subfolder in that folder for each sequence
+# Creates a folder for each action and creates a subfolder in that folder for each sequence
 for action in actions:
     for sequence in range(no_sequences):
         try:
--- main/Visions.py
+++ main/Visions.py
@@ -20,30 +20,6 @@ def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)#Draw left hand connections
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)#Draw right hand connections
 
-cap = cv.VideoCapture(1)
-#Set mediapipe model
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened():
-
-        #Read Frame
-        ret, frame = cap.read()
-
-        #Make Detections
-        image, results = mediapipe_detection(frame, holistic)
-        #print(results)
-
-        #Draw landmarks
-        draw_landmarks(image, results)
-
-        #Show frame     
-        cv.imshow('Hand Tracking', image)
-
-        #If esc is pressed break
-        if cv.waitKey(10) & 0xFF == 27:
-            break
-    cap.release()
-    cv.destroyAllWindows()
-
 def extract_keypoints(results):
     right_hand = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
     left_hand = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
@@ -52,19 +28,69 @@ def extract_keypoints(results):
 #Path for exported data (np arrays)
 DATA_PATH = os.path.join('MP_Data')
 
-#Actions we try to detect
+# Actions we try to detect
 actions = np.array(['rock', 'paper', 'scissors'])
 
-#Action Detection - rather than one frame I used 30 video worth of data to predict actions 
+# Action Detection - rather than one frame I used 30 video worth of data to predict actions 
 no_sequences = 30
 
-#Videos are going to be 30 frames in length
+# Videos are going to be 30 frames in length
 sequence_length = 30
 
-#Creates a folder for each action and creates a subfolder in that folder for each sequence
+# Creates a folder for each action and creates a subfolder in that folder for each sequence
 for action in actions:
     for sequence in range(no_sequences):
         try:
             os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
         except:
             pass
+
+cap = cv.VideoCapture(1)
+#Set mediapipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    #while cap.isOpened():
+    #Loop through actions
+    for action in actions:
+        #Loop through sequences(videos)
+        for sequence in range(no_sequences):
+            #Loop through video length(sequence length)
+            for frame_num in range(sequence_length):
+
+                #Read Frame
+                ret, frame = cap.read()
+
+                #Make Detections
+                image, results = mediapipe_detection(frame, holistic)
+                #print(results)
+
+                #Draw landmarks
+                draw_landmarks(image, results)
+
+                #Apply Wait Logic
+                if frame_num == 0:
+                    cv.putText(image, 'Starting Collection', (120, 200), 
+                               cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv.LINE_AA)
+                    cv.putText(image, 'Collecting frames for {} Video Number {}'.format(actions, sequence), (15, 12),
+                               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv.LINE_AA)
+                    #Show frame   
+                    cv.imshow('Hand Tracking', image)
+                    cv.resizeWindow('Hand Tracking', 1250, 800)
+                    cv.waitKey(4000)
+                else:
+                    cv.putText(image, 'Collectig frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv.LINE_AA)
+                    #Show frame   
+                    cv.imshow('Hand Tracking', image)
+                    cv.resizeWindow('Hand Tracking', 1250, 800)
+
+                #Export Keypoints
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
+
+                #If esc is pressed break
+                if cv.waitKey(10) & 0xFF == ord('q'):
+                    break
+            cap.release()
+            cv.destroyAllWindows()
+
